0424a69ad1fe207e82520b4c7ca3e20b05977873
gustavo <gustavo.soares.cdc@gmail.com>
2020-08-19 20:54:15 -0300

    [19/08/2020][ :art: ] static files

---
+++ static/dist/js/global/default.js
@@ -0,0 +1,16 @@
+// all modals reload
+$(".modal").on('hidden.bs.modal', function () {
+    $(this).find('.modal-content')
+        .html("<div class=\"modal-body\">\n" +
+        "          <i class=\"fas fa-sync fa-spin loading-img\"></i>\n" +
+        "          <span> &nbsp;&nbsp;Carregando…</span>\n" +
+        "        </div>")
+});
+
+// all datepickers
+$('.date').datepicker({
+  daysOfWeekHighlighted: '06',
+  format: 'dd/mm/yyyy',
+  todayHighlight: true,
+  autoclose: true
+});
