39d5b84b457e6c6eb751175151c16aec088d69dd
gustavo <gustavo.soares.cdc@gmail.com>
2020-09-26 14:18:09 -0300

    [26/09/2020][ :tada: ] arquivos de deploy

---
+++ run/start_gunicorn.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+NAME="seu-projeto"                              #Name of the application (*)
+DJANGODIR=/home/gustavo/workspace/env/SmartEvent             # Django project directory (*)
+SOCKFILE=/home/gustavo/workspace/env/SmartEvent/run/gunicorn.sock        # we will communicate using this unix socket (*)
+USER=gustavo                                       # the user to run as (*)
+GROUP=webdata                                     # the group to run as (*)
+NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
+DJANGO_SETTINGS_MODULE=smart_event.settings.development             # which settings file should Django use (*)
+DJANGO_WSGI_MODULE=smart_event.wsgi                     # WSGI module name (*)
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+#source /var/www/seu_projeto/venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user $USER \
+  --bind=unix:$SOCKFILE
+
+IMPORTANTE!
+DNS: 213.136.95.10
+DNS: 213.136.95.11
