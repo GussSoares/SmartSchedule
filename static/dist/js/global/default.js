4f95b8bd8402012a9f9488450b227022e963b085
gustavo <gustavo.soares.cdc@gmail.com>
2020-09-30 22:54:19 -0300

    [30/09/2020][ :tada: ] adicionando API ViaCep

--- static/dist/js/global/default.js
+++ static/dist/js/global/default.js
@@ -21,6 +21,21 @@ $('.select2').select2({
     allowClear: true
 })
 
+// autocomplete CEP
+$('#id_cep').blur(function () {
+    if ($(this).val() !== "") {
+        $.ajax({
+            url: `https://viacep.com.br/ws/${$(this).val()}/json/`,
+        }).then(function (data) {
+            $('#id_logradouro').val(data.logradouro)
+            $('#id_numero').val(data.complemento)
+            $('#id_bairro').val(data.bairro)
+            $('#id_cidade').val(data.localidade)
+            $('#id_uf').val(data.uf)
+        })
+    }
+})
+
 
 // function to create element by html string passed
 function createElementFromHTML(htmlString) {
