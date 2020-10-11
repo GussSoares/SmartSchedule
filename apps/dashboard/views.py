194ed1db6f60ebf535508bd2454478c47bd5956e
gustavo <gustavo.soares.cdc@gmail.com>
2020-10-11 08:00:32 -0300

    [11/10/2020][ :tada: ] correcoes no script e adicao de novas informacoes

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
-from ..cliente.models import Client, Member, Coordinator
+from ..cliente.models import Client, Member, Coordinator, Group
 from ..escala.models import Schedule
 from ..notification.models import Commentary
 from ..core.utils import timezone, today
@@ -18,25 +18,25 @@ def index(request):
         # membros adicionados nos ultimos 15 dias
         active_members = Member.objects.filter(cliente__is_active=True, grupo=group).count()
         last_members = Member.objects.filter(cliente__created_at__gte=today()-datetime.timedelta(days=15), grupo=group).order_by('-cliente__created_at')
-        schedules = Schedule.objects.filter(schedulemember__membro__grupo=group).distinct()
-        commentaries = Commentary.objects.filter(membro__grupo=group)
+        schedules = Schedule.objects.filter(schedulemember__membro__grupo=group).distinct().count()
+        groups = []
     elif request.user.is_superuser:
         active_members = Member.objects.filter(cliente__is_active=True).count()
         last_members = Member.objects.filter(cliente__created_at__gte=(today()-datetime.timedelta(days=15))).order_by('-cliente__created_at')
-        schedules = Schedule.objects.all()
+        schedules = Schedule.objects.all().count()
+        groups = Group.objects.all().count()
         # comentarios dos ultimos 15 dias
-        commentaries = Commentary.objects.filter(created_at__gte=today()-datetime.timedelta(days=15))
     else:
         last_members = []
         schedules = []
         active_members = []
-        commentaries = []
+        groups = []
 
     context = {
         'user': request.user,
         'active_members': active_members,
         'last_members': last_members,
         'schedules': schedules,
-        'commentaries': commentaries
+        'groups': groups
     }
     return render(request, 'dashboard/index.html', context)
--- apps/notification/scripts/send_notification.py
+++ apps/notification/scripts/send_notification.py
@@ -20,24 +20,31 @@ def get_payload(player_ids, title, message, schedule_id):
             "include_player_ids": player_ids,
             "contents": {"en": message},
             "headings": {"en": title},
-            "web_push_topic": schedule_id
+            "web_push_topic": schedule_id,
+            "url": "https://teste.smartschedule.ml/acl/login-confirm"
             }
 
 
 def run(*args):
+    print("[{}] - RUN ==> Running Script Send Notifications...".format(datetime.datetime.now()))
     schedules = Schedule.objects.filter(inicio__gte=default_ini, fim__lte=default_end)
 
     for schedule in schedules:
-        members = schedule.schedulemember_set.all()
-        player_ids = list(members.values_list('membro__cliente__player_id', flat=True))
+        try:
+            members = schedule.schedulemember_set.all()
+            player_ids = list(members.values_list('membro__cliente__player_id', flat=True))
 
-        msg = "Você está escalado para hoje às {}h.".format(schedule.inicio.astimezone(timezone).strftime("%H:%M"))
-        title = "Eai! Cuida na Escala. 📅"
-        header = get_header()
-        # payload = get_payload(['5d10d117-f413-4cee-82c5-5724bd949125'], title, msg)
-        payload = get_payload(player_ids, title, msg, schedule.id)
-        print('==> Sending Notification to {}...'.format(player_ids))
-        req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
-        print(req)
+            msg = "Você está escalado para hoje às {}h.".format(schedule.inicio.astimezone(timezone).strftime("%H:%M"))
+            title = "Eai! Cuida na Escala. 📅"
+            header = get_header()
+            # payload = get_payload(['5d10d117-f413-4cee-82c5-5724bd949125'], title, msg)
+            payload = get_payload(player_ids, title, msg, schedule.id)
+            print('[{}] - NOTIFY ==> Sending Notification to {}...'.format(datetime.datetime.now(), player_ids))
+            req = requests.post("https://onesignal.com/api/v1/notifications", headers=header, data=json.dumps(payload))
+            print(req)
+
+            # todo: salvar log no banco de dados para cada notificacao enviada
+        except Exception as exc:
+            print("[{}] - ERROR ==> Erro ao enviar notificacao: {}".format(datetime.datetime.now(), str(exc)))
 
 
